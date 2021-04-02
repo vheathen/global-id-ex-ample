b004738b0bee7e8b0b5ac3f8f3e2ccf382e69733
heathen <v.heathen@gmail.com>
2021-04-02 18:23:55 +0500

    GlobalId

--- lib/global_id.ex
+++ lib/global_id.ex
@@ -1,18 +1,107 @@
 defmodule GlobalId do
   @moduledoc """
-  Documentation for `GlobalId`.
+  GlobalId module contains an implementation of a guaranteed globally unique id system.
+
+  Current implementation is able to generate up to 2_048_000 ids per second per process
+  and should guarantee id uniqueness until 2248-09-26 15:10:22.208 UTC (43 bits timestamp).
+  Varying timestamp/counter bit length it is possible to change maximum generation performance.
   """
+
+  @node_id :random.uniform(1024) - 1
+  @endless_retries true
+
+  @timestamp_length 43
+  @counter_length 11
+  @counter_max 2047
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GlobalId.hello()
-      :world
-
+  Please implement the following function.
+  64 bit non negative integer output
   """
-  def hello do
-    :world
+  @spec get_id(non_neg_integer) :: non_neg_integer
+  def get_id(last_id), do: do_get_id(<<last_id::64>>, timestamp_bt())
+
+  #
+  # You are given the following helper functions
+  # Presume they are implemented - there is no need to implement them.
+  #
+
+  @doc """
+  Returns your node id as an integer.
+  It will be greater than or equal to 0 and less than or equal to 1023.
+  It is guaranteed to be globally unique.
+  """
+  @spec node_id() :: non_neg_integer
+  def node_id, do: Process.get(:node_id, @node_id)
+
+  @doc """
+  Returns timestamp since the epoch in milliseconds.
+  """
+  @spec timestamp() :: non_neg_integer
+  def timestamp, do: System.system_time(:millisecond)
+
+  #
+  # Private API
+  #
+
+  #
+  # If we request for a new id within the same time slot:
+  #
+  # We can have max 2^11 = 2048 ids per millisecond so if we
+  # got counter == 2047 (0..2047) then we need to sleep till the
+  # next timeslot or retry immediately
+  #
+  defp do_get_id(
+         <<current_timestamp::@timestamp_length, @counter_max::@counter_length, _node_id::10>> =
+           last_id,
+         <<current_timestamp::@timestamp_length>>
+       ) do
+    unless @endless_retries, do: Process.sleep(1)
+    do_get_id(last_id, timestamp_bt())
   end
+
+  #
+  # Just increment counter within the current timeslot and return a new id
+  #
+  defp do_get_id(
+         <<current_timestamp::@timestamp_length, counter::@counter_length, _node_id::10>>,
+         <<current_timestamp::@timestamp_length>>
+       ),
+       do: return_id(current_timestamp, counter + 1)
+
+  #
+  # This shouldn't happens but if we've got an id from future (for example,
+  # because of the timestamp generator source time drift) we need to wait till
+  # that future to ensure the next ids are unique.
+  #
+  defp do_get_id(
+         <<future_timestamp::@timestamp_length, _counter::@counter_length, _node_id::10>> =
+           last_id,
+         <<current_timestamp::@timestamp_length>>
+       )
+       when future_timestamp > current_timestamp do
+    Process.sleep(future_timestamp - current_timestamp)
+    do_get_id(last_id, timestamp_bt())
+  end
+
+  #
+  # And if the last id timestamp from the past we can reset counter to zero
+  #
+  defp do_get_id(
+         <<past_timestamp::@timestamp_length, _counter::@counter_length, _node_id::10>>,
+         <<current_timestamp::@timestamp_length>>
+       )
+       when past_timestamp < current_timestamp,
+       do: return_id(current_timestamp, 0)
+
+  defp return_id(timestamp, counter) do
+    return_id(<<timestamp::@timestamp_length, counter::@counter_length, node_id()::10>>)
+  end
+
+  #
+  # Let's make an intereger from a bitstring and return it
+  #
+  defp return_id(<<result::64>>), do: result
+
+  defp timestamp_bt, do: <<timestamp()::@timestamp_length>>
 end
--- test/global_id_test.exs
+++ test/global_id_test.exs
@@ -2,7 +2,41 @@ defmodule GlobalIdTest do
   use ExUnit.Case
   doctest GlobalId
 
-  test "greets the world" do
-    assert GlobalId.hello() == :world
+  @ids_per_node 1_000_000
+  @nodes 3
+  @total_ids_count @ids_per_node * @nodes
+
+  test "should create unique global ids" do
+    total_ids =
+      1..@nodes
+      |> Enum.reduce([], fn _, nodes -> [gen_node_id(nodes) | nodes] end)
+      |> Enum.map(&Task.async(fn -> generate_ids(&1, @ids_per_node) end))
+      |> Enum.map(&Task.await(&1, 60_000))
+      |> List.flatten()
+
+    assert @total_ids_count == length(total_ids)
+    assert @total_ids_count == length(Enum.uniq(total_ids))
+  end
+
+  defp gen_node_id(node_ids) do
+    id = :random.uniform(1024) - 1
+
+    if id in node_ids,
+      do: gen_node_id(node_ids),
+      else: id
+  end
+
+  defp generate_ids(node_id, amount) do
+    Process.put(:node_id, node_id)
+
+    for _ <- 1..amount, reduce: [] do
+      [] ->
+        [GlobalId.get_id(0)]
+
+      [last_id | _] = ids ->
+        new_id = GlobalId.get_id(last_id)
+        assert last_id < new_id
+        [new_id | ids]
+    end
   end
 end
